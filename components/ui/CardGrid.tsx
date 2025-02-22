027f98a9da3ed1a836680a51601916d755387e7f
GopiWebDev <gopisv.webdev@gmail.com>
2025-02-22 12:46:47 +0530

    fixed type check errors

--- components/ui/CardGrid.tsx
+++ components/ui/CardGrid.tsx
@@ -1,12 +1,17 @@
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-nocheck
-
 import ProductCard from "./ProductCard";
 
-const CardGrid = ({ data }: { data: unknown }) => {
+type Item = {
+  image: string;
+  title: string;
+  rating: number;
+  amount: number;
+  discountPercent: number;
+};
+
+const CardGrid = ({ data }: { data: Item[] }) => {
   return (
     <div className="hidden w-full md:grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
-      {data.map((item: object, index: number) => {
+      {data.map((item: Item, index: number) => {
         if (index < 4)
           return <ProductCard key={item.title + index} item={item} />;
       })}
