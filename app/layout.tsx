5c608d043047645eca7f9baf8de1eeae18191640
GopiWebDev <gopisv.webdev@gmail.com>
2025-02-24 16:47:38 +0530

    fixed issue by renaming file

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import localFont from "next/font/local";
 import NotificationBar from "@/components/layout/sections/home/NotificationBar";
-import Navbar from "@/components/layout/navbar/Navbar";
+import Navbar from "@/components/layout/Navbar/Navbar";
 import Footer from "@/components/layout/Footer/Footer";
 import FetchProducts from "@/components/FetchProducts";
 
