34317ed5cf9fea4f3968cc6c3af93454a155e6d4
GopiWebDev <gopisv.webdev@gmail.com>
2025-03-07 13:56:53 +0530

    fixed invalid params issue

--- app/shop/[id]/page.tsx
+++ app/shop/[id]/page.tsx
@@ -1,8 +1,15 @@
 import SingleProduct from "@/components/layout/sections/shop/SingleProduct";
 
-export default async function Page({ params }: { params: { id: string } }) {
-  const { id } = params;
-  const productId = id.split("-")[0];
+export default async function Page({ params }: { params: { id?: string } }) {
+  console.log("params:", params); // Debugging
 
-  return <SingleProduct id={parseInt(productId)} />;
+  if (!params?.id) return <div>Product not found</div>;
+
+  const productId = params?.id.split("-")[0];
+
+  if (!/^\d+$/.test(productId)) {
+    return <div>Invalid product ID</div>;
+  }
+
+  return <SingleProduct id={Number(productId)} />;
 }
